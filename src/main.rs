3e96b1bccf623428d14733d9e631d339e87febf9
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-20 12:59:54 -0400

    src/main.rs:paths:home about

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use tejmagar::paths::{Path, Paths};
+use tejmagar::request::Request;
+use tejmagar::response::Response;
+use tejmagar::server::run_server;
+use tejmagar::status::Status;
+
+fn home(_request: Request, mut response: Response) {
+    response.html(Status::Ok, "<a href=\"/\">Home</a> <a href=\"/about\">About</a>".to_string()).send();
+}
+
+fn about(_request: Request, mut response: Response) {
+    response.html(Status::Ok, "<a href=\"/\">Home</a> <a href=\"/about\">About</a>".to_string()).send();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let paths: Paths = vec![
+        Path::new("/", home),
+        Path::new("/about", about),
+    ];
+
+    run_server("0.0.0.0:8080", paths);
 }
