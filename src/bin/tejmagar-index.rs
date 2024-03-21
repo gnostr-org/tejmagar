df6723d40e4e27d45070f4da76b4ca37aabf2817
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-21 00:57:59 -0400

    src/bin/tejmagar-index.rs:HOME CONTENT and ABOUT CONTENT

--- src/bin/tejmagar-index.rs
+++ src/bin/tejmagar-index.rs
@@ -4,21 +4,35 @@ use tejmagar::response::Response;
 use tejmagar::server::run_server;
 use tejmagar::status::Status;
 
-
-static HOME_HTML: &'static str = "
-
+static BOOTSTRAP_CSS: &'static str = "
 <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN\" crossorigin=\"anonymous\">
-
-<a href=\"/\">HOME_HTML Home</a> <a href=\"/about\">About</a>
-
-
+";
+static HOME_HTML: &'static str = "
+<div class=\"container-fluid\">
+<a class=\"navbar-brand\" href=\"#\">
+<a href=\"/\">Home</a> <a href=\"/about\">About</a>
+</a>
+<div class=\"position-fixed mb-3 me-3 bd-mode-toggle\">
+HOME CONTENT
+</div>
+</div>
+";
+static ABOUT_HTML: &'static str = "
+<div class=\"container-fluid\">
+<a class=\"navbar-brand\" href=\"#\">
+<a href=\"/\">Home</a> <a href=\"/about\">About</a>
+</a>
+<div class=\"position-fixed mb-3 me-3 bd-mode-toggle\">
+ABOUT CONTENT
+</div>
+</div>
 ";
 
 fn home(_request: Request, mut response: Response) {
     response
         .html(
             Status::Ok,
-            HOME_HTML.to_string(),
+            format!("{}{}", BOOTSTRAP_CSS.to_string(), HOME_HTML.to_string()),
         )
         .send();
 }
@@ -27,7 +41,7 @@ fn about(_request: Request, mut response: Response) {
     response
         .html(
             Status::Ok,
-            "<a href=\"/\">Home</a> <a href=\"/about\">About</a>".to_string(),
+            format!("{}{}", BOOTSTRAP_CSS.to_string(), ABOUT_HTML.to_string()),
         )
         .send();
 }
