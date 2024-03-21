2cd3076351ac5493c313bd0fbb0a10ff07cea122
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-03-21 00:27:53 -0400

    src/bin/tejmagar-index.rs:bootstrap css

--- src/bin/tejmagar-index.rs
+++ src/bin/tejmagar-index.rs
@@ -4,11 +4,21 @@ use tejmagar::response::Response;
 use tejmagar::server::run_server;
 use tejmagar::status::Status;
 
+
+static HOME_HTML: &'static str = "
+
+<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN\" crossorigin=\"anonymous\">
+
+<a href=\"/\">HOME_HTML Home</a> <a href=\"/about\">About</a>
+
+
+";
+
 fn home(_request: Request, mut response: Response) {
     response
         .html(
             Status::Ok,
-            "<a href=\"/\">Home</a> <a href=\"/about\">About</a>".to_string(),
+            HOME_HTML.to_string(),
         )
         .send();
 }
